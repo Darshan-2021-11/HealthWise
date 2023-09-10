ad002fe36e084cd6efdab5b2516d3576622e4487
AMIT ROUT <120554331+AmitRout1704@users.noreply.github.com>
2023-09-10 15:38:14 +0530

    user dashboard and upload prescution

---
+++ frontend/src/components/UploadPrescutionNav/UploadPrescutionNav.js
@@ -0,0 +1,73 @@
+import React, { useRef, useState } from "react";
+
+// import Upload1 from "../Home/img/upload1.png";
+import Upload11 from "../Home/img/submit.png";
+import axios from "axios";
+import UploadPrescutionCards from "./UploadPrescutionCards";
+import MedicineCard from "../Home/MedicineCard";
+import "./upload.css";
+const UploadPrescutionNav = () => {
+  const [files, setFiles] = useState(null);
+  const [medicine, setmedicine] = useState();
+  const bottomRef = useRef(null);
+
+  const scanPrescution = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append("files", files);
+    await axios
+      .post("http://localhost:4000/api/v1/search/medicine", formData, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data.result.medicines);
+        setmedicine(res.data.result.medicines);
+        bottomRef.current.scrollIntoView({ behavior: "smooth" });
+      })
+      .catch((err) => {
+        alert("some error occured while scanning, pls try manually");
+        console.log(err);
+      });
+  };
+
+  return (
+    <div ref={bottomRef}>
+      <div class="wrapper1">
+        <div className="indiv1">
+          <div className="upload-your-prec">
+            <h2>UPLOAD YOUR PRESCIPTION</h2>
+            <p>Upload prescution for get your desired medicine you want</p>
+          </div>
+          <label htmlFor="fileBTN-upload" className="drop-container">
+            <img src={Upload11} alt="" />
+            <span className="drop-title">Drop files here</span>
+            <input
+              type="file"
+              id="fileBTN-upload"
+              accept="image/*"
+              onChange={(e) => {
+                e.preventDefault();
+
+                setFiles(e.target.files[0]);
+              }}
+            />
+          </label>
+          <button className="button" onClick={scanPrescution}>
+            Search Medicine
+          </button>
+        </div>
+      </div>
+
+      {medicine && medicine.length === 0 ? (
+        <p>no medicine found</p>
+      ) : (
+        <div className="medicine-item">
+          {medicine && medicine.map((item) => <MedicineCard data={item} />)}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UploadPrescutionNav;
---
+++ frontend/src/components/userDashboard/Sidebar.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Dashboard.css";
+import UserNotVerified from "./img/usernotverified.png";
+import UserVerified from "./img/userverified.png";
+import UpdateUser from "./img/updateuser.png";
+import UserIcon from "./img/usericon.png";
+import MedicineTaken from "./img/medicinetaken.png";
+import MedicineUnavailable from "./img/medicineunavailable.png";
+import MedicineEmergency from "./img/medicineemergency.png";
+import LabTest from "./img/labTest.png";
+
+function Sidebar() {
+  const id = localStorage.getItem("userId");
+  const username = localStorage.getItem("userName");
+	const userVerified = localStorage.getItem("userVerified");
+  const iconStyle = { width: "30px", height: "30px" };
+	console.log(userVerified, typeof userVerified);
+	const verified = userVerified == "false" ?
+    {
+      title: "Not Verified",
+      link: `/dashboard/user/verifyyourself/${id}`,
+      icon: UserNotVerified,
+      iconStyle: iconStyle,
+    } :
+    {
+      title: "Account Verified",
+      link: `#`,
+      icon: UserVerified,
+      iconStyle: iconStyle,
+    };
+  const sidebarData = [
+		verified,
+    {
+      title: "Update Profile",
+      link: `/dashboard/user/updateUser/${id}`,
+      icon: UpdateUser,
+      iconStyle: iconStyle,
+    },
+    {
+      title: "Medicines Taken",
+      link: `/dashboard/user/medicineTaken/${id}`,
+      icon: MedicineTaken,
+      iconStyle: iconStyle,
+    },
+  ];
+
+  return (
+    <div className="navigationPanel dashboardMenu-child">
+      <ul className="sidebarList">
+        <li
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "25px 0",
+          }}
+        >
+          <img
+            src={UserIcon}
+            style={{ width: "80px", height: "80px" }}
+            alt=""
+          />
+          <div style={{ fontWeight: "600", fontSize: "26px" }}>{username}</div>
+        </li>
+        {sidebarData.map((item, key) => {
+          const activeClass =
+            window.location.pathname === item.link
+              ? "activeLink"
+              : "sidebarListItem";
+          return (
+            <Link to={item.link} className={activeClass} key={key}>
+              <div>
+                <div className="flexbox-column">
+                  <img src={item.icon} style={item.iconStyle} alt=" " />
+                  <li>{item.title}</li>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
